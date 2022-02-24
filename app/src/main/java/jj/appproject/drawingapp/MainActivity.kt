acec23f2c4228e1ab5406ae34d89b4b21fe9bb52
[jsh1304] <[jung3914a@gmail.com]>
2022-02-24 21:49:23 +0900

    initial commit

---
+++ app/src/main/java/jj/appproject/drawingapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package jj.appproject.drawingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
