31237986653134d0fb1300adb4f176a3aed9f4bd
[jsh1304] <[jung3914a@gmail.com]>
2022-02-25 13:39:37 +0900

    220225-2

--- app/src/main/java/jj/appproject/drawingapp/DrawingView.kt
+++ app/src/main/java/jj/appproject/drawingapp/DrawingView.kt
@@ -86,11 +86,7 @@ class DrawingView(context: Context, attrs:AttributeSet) : View(context,attrs){
                 }
             }
             MotionEvent.ACTION_MOVE -> {
-                if (touchX != null) {
-                    if (touchY != null) {
-                        mDrawPath!!.lineTo(touchX, touchY)
-                    }
-                }
+                 mDrawPath!!.lineTo(touchX!!, touchY!!)
             }
             MotionEvent.ACTION_UP -> {
                 mPaths.add(mDrawPath!!)
