52f5ffee801908dc7d9f2835a394e89e4021c04e
[jsh1304] <[jung3914a@gmail.com]>
2022-02-26 21:43:53 +0900

    220226

--- app/src/main/java/jj/appproject/drawingapp/MainActivity.kt
+++ app/src/main/java/jj/appproject/drawingapp/MainActivity.kt
@@ -1,16 +1,21 @@
 package jj.appproject.drawingapp
 
 import android.app.Dialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
 
-    private  var drawingView: DrawingView? = null
+    private var drawingView: DrawingView? = null
 
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.palette_pressed)
+                )
 
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener{
